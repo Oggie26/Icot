32216fb1e7410d4ddfebf78e1abef57a8e5739a7
Oggie26 <146812741+Oggie26@users.noreply.github.com>
2025-07-22 15:43:55 +0700

    fix

--- src/main/java/Project/example/Project_1/config/SecurityConfig.java
+++ src/main/java/Project/example/Project_1/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration cors = new CorsConfiguration();
-        cors.setAllowedOrigins(List.of("https://icot.vercel.app", "https://icot.vercel.app/, "http://localhost:5173"));
+cors.setAllowedOrigins(List.of("https://icot.vercel.app", "https://icot.vercel.app/", "http://localhost:5173"));
         cors.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE","PATCH", "OPTIONS"));
         cors.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         cors.setAllowCredentials(true);
