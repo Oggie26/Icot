4dc022250a571ed45ea905de0f9453c4e8152ca9
Oggie26 <146812741+Oggie26@users.noreply.github.com>
2025-08-07 17:57:57 +0700

    fix sv

--- src/main/java/Project/example/Project_1/config/Filter.java
+++ src/main/java/Project/example/Project_1/config/Filter.java
@@ -38,6 +38,7 @@ public class Filter extends OncePerRequestFilter {
             "/swagger-resources/**",
             "/api/auth/login",
             "/api/auth/register",
+            "/api/auth/ping",
             "/api/auth/register/mobile",
             "/api/user/{id}",
             "/api/products",
